4d20fec03432a78001994b52187c9bf96a2c237d
RayRay <donsaylazy52@gmail.com>
2020-06-30 10:51:24 +0800

    azure function app demo init

--- OnPaymentReceived.cs
+++ OnPaymentReceived.cs
@@ -15,12 +15,17 @@ namespace azFunctionDemo
         [FunctionName("OnPaymentReceived")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            [Queue("orders")] IAsyncCollector<Order> orderQueue,
             ILogger log)
         {
             log.LogInformation("Received a payment.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            var order = JsonConvert.DeserializeObject<Order>(requestBody);
+
+            await orderQueue.AddAsync(order);
+
+            log.LogInformation($"Order {order.OrderId} received from {order.Email} for product {order.ProductId}");
 
             return new OkObjectResult($"Thank you for your purchase");
         }
---
+++ Order.cs
@@ -0,0 +1,7 @@
+namespace azFunctionDemo {
+    public class Order {
+        public string OrderId { get; set; }
+        public string Email { get; set; }
+        public string ProductId { get; set; }
+    }
+}
